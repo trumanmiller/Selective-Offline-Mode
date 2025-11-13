32a40b2882aab355e92ef542ddbd3d57688bf3a9
truman <mail@trumanmiller.com>
2025-11-12 22:29:53 -0600

    update to 1.21.10

--- src/main/java/jmeow/selectiveofflinemode/mixin/PlayerManagerMixin.java
+++ src/main/java/jmeow/selectiveofflinemode/mixin/PlayerManagerMixin.java
@@ -1,14 +1,10 @@
 package jmeow.selectiveofflinemode.mixin;
 
-import com.mojang.authlib.GameProfile;
 import jmeow.selectiveofflinemode.NameExpiry;
+import net.minecraft.server.PlayerConfigEntry;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,8 +15,9 @@ import java.util.Date;
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
     @Inject(method = "checkCanJoin", at = @At(value = "RETURN"), cancellable = true)
-    private void checkCanJoin(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Text> cir) {
-        if (NameExpiry.names.containsKey(profile.getName()) && NameExpiry.names.get(profile.getName()).after(new Date())) {
+    private void checkCanJoin(SocketAddress address, PlayerConfigEntry profile, CallbackInfoReturnable<Text> cir) {
+
+        if (NameExpiry.names.containsKey(profile.name()) && NameExpiry.names.get(profile.name()).after(new Date())) {
             cir.setReturnValue(null);
         } else {
             cir.setReturnValue(cir.getReturnValue());
--- src/main/java/jmeow/selectiveofflinemode/mixin/ServerMixin.java
+++ src/main/java/jmeow/selectiveofflinemode/mixin/ServerMixin.java
@@ -2,40 +2,28 @@ package jmeow.selectiveofflinemode.mixin;
 
 import com.mojang.authlib.GameProfile;
 import jmeow.selectiveofflinemode.NameExpiry;
-import net.fabricmc.loader.impl.util.log.Log;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
 import org.jetbrains.annotations.Nullable;
-import org.objectweb.asm.Opcodes;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
-import static java.util.Map.entry;
 
 @Mixin(ServerLoginNetworkHandler.class)
 public class ServerMixin {
-	@Shadow
-	@Nullable
-	GameProfile profile;
-
 	@Shadow @Final
     static Logger LOGGER;
 
-	@Unique
+    @Shadow
+    @Nullable String profileName;
+    @Unique
 	private static final Map<String, Date> nameExpiry;
 
 	static {
@@ -44,12 +32,11 @@ public class ServerMixin {
 
 	@Redirect(method = "onHello", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isOnlineMode()Z"))
 	private boolean isOnlineMode(MinecraftServer instance) {
-        if (profile != null) {
-			if(NameExpiry.names.containsKey(profile.getName()) && NameExpiry.names.get(profile.getName()).after(new Date())) {
-				LOGGER.info("{} joined with permission", profile.getName());
+        String username = this.profileName;
+			if(NameExpiry.names.containsKey(username) && NameExpiry.names.get(username).after(new Date())) {
+				LOGGER.info("{} joined with permission", username);
 				return false;
 			}
-        }
         return true;
 	}
 }
